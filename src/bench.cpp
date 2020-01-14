8a29cb64e82ab3327034ed8d47c1d7ee95b1d5fe
Nicolas Roger <nicolas.roger@epita.fr>
2020-01-14 21:14:42 +0100

    bench

--- src/bench.cpp
+++ src/bench.cpp
@@ -1,6 +1,7 @@
 #include "render.hpp"
 #include <vector>
 #include <benchmark/benchmark.h>
+#include <iostream>
 
 unsigned width = 1200;
 unsigned height = 600;
@@ -9,12 +10,20 @@ constexpr int kRGBASize = 4;
 int stride = width * kRGBASize;
 auto buffer = std::make_unique<std::byte[]>(height * stride);
 
-void BM_Rendering_gpu(benchmark::State& st)
+static void BM_Rendering_gpu(benchmark::State& st)
 {
+  unsigned p = 0;
   // Rendering
   for(auto _:st)
+  {
+    p++;
     render(reinterpret_cast<char*>(buffer.get()), width, height, ns, stride, 32);
+  }
+  //st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+  std::cout << p << std::endl;
+  st.counters["p"] = p;
   st.counters["frame_rate"] = benchmark::Counter(st.iterations(), benchmark::Counter::kIsRate);
+  st.counters["test"] = benchmark::Counter(st.real_time(), benchmark::Counter::kAvgThreads);
 }
 
 BENCHMARK(BM_Rendering_gpu)
